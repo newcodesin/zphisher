50572db9c58f6b8b6e038de339ce3377f2ea7f87
Nithish <94756343+newcodesin@users.noreply.github.com>
2023-04-09 16:04:42 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -212,7 +212,7 @@ check_status() {
     if ! [ -x "$(command -v toilet)" ]; then
         echo -e "\e[4;34m This Bomber Was Created By \e[1;NITHISH \e[0m"
     else
-        echo -e "\e[1;34mCreated By \e[1;34m"
+        echo -e "\e[1;20mCreated By \e[1;20m"
         toilet -f mono12 -F border BNR
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
